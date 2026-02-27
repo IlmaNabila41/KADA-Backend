7bfabbd3a580beb3349f971e3ba22cd2690291c3
IlmaNabila41 <ilma.nabila63@gmail.com>
2026-02-27 16:57:56 +0700

    delete leaked mongo uri

--- index.js
+++ index.js
@@ -16,23 +16,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 export default app;
-
-// import { MongoClient } from 'mongodb';
-
-// const uri = "mongodb+srv://root:root@cluster0.nipajpl.mongodb.net/?appName=Cluster0";;
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   await client.connect();
-//   console.log("Connected to MongoDB!");
-
-//   const db = client.db("belajar");
-//   const users = db.collection("users");
-
-//   await users.insertOne({ nama: "Kirman", umur: 100 });
-
-//   const data = await users.find().toArray();
-//   console.log(data);
-// }
-
-// run();
